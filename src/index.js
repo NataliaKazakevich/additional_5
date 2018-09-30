50e0717ce3d63f495d43e4ea32694fbf40897b0c
Natalia Kazakevich <Natallia_Kazakevich@mail.ru>
2018-09-30 21:52:18 +0300

    task 6

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  //функция ES6
+  
+  let stack=[];
+  let open=[];
+  let close=[];
+
+  for (let i = 0, lenBrackets =bracketsConfig.length; i < lenBrackets; i++) {
+    open.push(bracketsConfig[i][0]);
+    close.push(bracketsConfig[i][1]);
+  }
+
+  for (let i = 0, letStr=str.length; i < letStr; i++) {
+   
+    if (close.indexOf(str[i]) != -1)
+    {
+      if (stack.length == 0) {
+        if (open.indexOf(str[i]) == -1) return false;
+        else {
+          stack.push(str[i]); 
+        }
+      } else {                
+        let temp = stack.pop();
+        if (open.indexOf(temp) != close.indexOf(str[i])) {
+          if (open.indexOf(str[i]) == -1) return false;
+          else {
+            stack.push(temp);
+            stack.push(str[i]);
+          }
+        }
+      }
+    } else {
+          stack.push(str[i]);
+    }
+  }
+
+  return  (stack.length == 0);
 }
